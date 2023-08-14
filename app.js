14880f85c7cb6b7c82c7c8c3b5af3a824a5ababd
MihirSumant <mihirsumant@gmail.com>
2023-08-14 20:11:23 +0530

    Put in script tag, tested outputting the time

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const timeRightNow = () => {
-    const now = new Date();
-    return console.log(now);
-}
+//     const timeRightNow = () => {
+//      const now = new Date();
+//      return console.log(now);
+//  }
 
-timeRightNow();
+let clockSection =  document.querySelector('h1');
+let timeIs = new Date();
+
+    console.log(timeIs);
+    console.log('Hurray');
+
+let word = 'Hello World';
+clockSection.innerHTML = `${timeIs}`;
